8b151fed7f75d063c65ed4bfb2c8ce574635f30d
JulioMorfin <julio.alsamo@gmail.com>
2024-10-16 17:23:32 -0600

    Density with dashboard demo.

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -11,6 +11,55 @@ Dashboards.PluginHandler.addPlugin(HighchartsPlugin)
 DataGridPlugin.custom.connectDataGrid(DataGrid)
 Dashboards.PluginHandler.addPlugin(DataGridPlugin)
 
+Highcharts.setOptions({
+  chart: {
+      type: 'area',
+      spacingTop: 20,
+      spacingBottom: 20
+  },
+  title: {
+      align: 'left',
+      margin: 0,
+      x: 30
+  },
+  credits: {
+      enabled: false
+  },
+  legend: {
+      enabled: false
+  },
+  xAxis: {
+      crosshair: true,
+      labels: {
+          format: '{value} km'
+      },
+      accessibility: {
+          description: 'Kilometers',
+          rangeDescription: '0km to 6.5km'
+      }
+  },
+  yAxis: {
+      title: {
+          text: null
+      }
+  },
+  tooltip: {
+      positioner: function () {
+          return {
+              // right aligned
+              x: this.chart.chartWidth - this.label.width,
+              y: 10 // align to title
+          };
+      },
+      borderWidth: 0,
+      backgroundColor: 'none',
+      pointFormat: '{point.y}',
+      headerFormat: '',
+      shadow: false,
+      valueDecimals: 0
+  }
+});
+
 export default function Dashboard(props) {
   const { config } = props
 
